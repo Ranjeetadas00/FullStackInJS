b1efc9e9a56f2f26b61ebdbe2bd476d7aad5d58a
Ranjeeta Das <100126029+Ranjeetadas00@users.noreply.github.com>
2025-08-21 00:54:35 +0000

    Arrays

---
+++ JavaScript/JsChallanges/p1Cond.js
@@ -0,0 +1,43 @@
+//checking if a no. is greater than snother no.
+let num1 = 7;
+let num2 =2;
+
+if(num1>num2){
+    console.log("num1 is greater")
+}
+else{
+    console.log("num2 is greater");
+    
+}
+
+//Checking if variable is number of not
+let score =" 45saf"
+if(typeof score === "number"){
+    console.log("It's a number");
+    
+}else {
+    console.log("not a Number");
+    
+}
+
+//Checking if boolean value is true or false
+let isTeaReady = true;
+
+if(isTeaReady){
+    console.log("Tea is Ready");
+    
+}else{
+    console.log("Not Ready");
+    
+}
+
+//checking if array is empty or not
+
+let items = [];
+if(items.length == 0){
+    console.log("Array is empty");
+    
+}else{
+    console.log("Array not empty");
+    
+}
