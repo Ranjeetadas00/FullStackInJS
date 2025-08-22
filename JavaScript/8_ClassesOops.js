7701a7d0614882c9c3cc4fc736e48f46c0065146
Ranjeeta Das <100126029+Ranjeetadas00@users.noreply.github.com>
2025-08-22 05:45:17 +0000

    oopsConcepts

--- JavaScript/8_ClassesOops.js
+++ JavaScript/8_ClassesOops.js
@@ -1,4 +1,4 @@
-
+// In JS Classes are just syntatic sugar over JS existing prototype based inheritance
 // =====================================Classes
 // 1)Inheritance
 class Vehical{
@@ -42,9 +42,63 @@ class BankAccount {
 
 let account  = new BankAccount();
 console.log(account.getbalance()); //$ 0
-console.log(account.balance);  //undefined
+// console.log(account.balance);  //undefined
 // console.log(account.#balance); //error as #balance is not accessible outside the class
 
 // ==================Abstraction
 // hides the complex impletation details
 
+class CoffeeMachine{
+    start(){
+        // call DB
+        // Filter values etc..
+        return `Starting machine.......`
+    }
+
+    brewCoffee(){
+        //complex calculations
+        return `Brewing Coffee..`
+    }
+    pressStartButton(){
+        let msg1=this.start();
+        let msg2=this.brewCoffee();
+        return `${msg1} and ${msg2}`
+    }
+}
+
+let myMachine = new CoffeeMachine();
+console.log(myMachine.pressStartButton())
+
+// ============================Polymorphism
+// same method name but different behaviour
+
+class Bird{
+    fly(){
+        return `Flaying.....`
+    }
+
+}
+class Penguin extends Bird{
+    fly(){
+        return `Penguins can't fly....`
+    }
+}
+
+let bird = new Bird()
+let penguin = new Penguin()
+console.log(bird.fly());
+console.log(penguin.fly());
+
+// ========================Static Methods and properties/variables
+// Static Methods and properties/variables are accessed only by class name -> ClassName.staticVer/method()
+
+class Calculator{
+    static add(a,b){
+        return a+b;
+    }
+}
+ let miniCals = new Calculator();
+ console.log(Calculator.add(4,5));
+//   console.log(miniCals.add(4,5));  // this will give error as add is a static methord and should be accessed only using Class Name
+ 
+ 
