2bd1991095bef9a549c81d49e82fcd7e9301a191
Ranjeeta Das <100126029+Ranjeetadas00@users.noreply.github.com>
2025-08-24 01:48:46 +0000

    ex-4,5

--- 9_DOMndBOM/script.js
+++ 9_DOMndBOM/script.js
@@ -10,3 +10,30 @@ document.getElementById('changeTextButton').addEventListener('click',function(){
 })
 
 //ex-2
+document.getElementById("highlightFirstCity").addEventListener("click",function(){
+    let hold = document.getElementById('citiesList')  //grab the element using id,
+    console.log(hold);
+    console.log(hold.firstElementChild);
+    hold.firstElementChild.classList.add("highlight")
+
+})
+
+//ex-3
+document.getElementById("changeOrder").addEventListener("click", function(){
+    let coffeeType = document.getElementById("coffeeType")
+    console.log(coffeeType);
+    coffeeType.textContent = "Expresso"
+    coffeeType.style.backgroundColor = "brown"
+    coffeeType.style.padding = "5px"
+    
+})
+
+// ex4
+document.getElementById("addNewItem").addEventListener("click",function(){
+    let newItem = document.createElement('li')
+    newItem.textContent = "Eggs"
+
+    document.getElementById("shoppingList").appendChild(newItem)
+})
+
+// ex-5
